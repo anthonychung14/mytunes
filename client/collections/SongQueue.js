df2ea32405d70b5f39104174ddf05e78698e1c14
anthonychung14 <anthonychung14@gmail.com>
2015-12-09 19:01:13 -0800

    Completed first half of tests

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,17 @@
 var SongQueue = Songs.extend({
 
   initialize: function() {
-  }
+    this.on('add', function(song) {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+  },
+
+
+
+  playFirst: function() {
+    console.log("yo");
+  } 
 
 });
--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,14 @@ var SongModel = Backbone.Model.extend({
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
-  }
+  },
 
+  enqueue: function() {
+    //set current song to this
+    //app.get('songQueue').push(this)
+    this.trigger('enqueue', this);
+  },
+  add: function() {
+    this.trigger('add', this);
+  }
 });
